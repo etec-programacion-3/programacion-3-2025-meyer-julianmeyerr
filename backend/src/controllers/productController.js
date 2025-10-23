ce0452a0dbf7b2ae4159036c45a4603638393390
julianmeyerr <j.meyer@alumno.etec.um.edu.ar>
2025-10-23 15:38:59 -0300

    issue 6: endpoints publicos de productos

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -1,4 +1,5 @@
 import Product from "../models/Product.js";
+import User from "../models/User.js";
 import asyncHandler from "express-async-handler";
 
 // Create Product
@@ -17,18 +18,38 @@ export const createProduct = asyncHandler(async (req, res) => {
 
 // Get product
 export const getProduct = asyncHandler(async (req, res) => {
-  const product = await Product.find();
-  res.json(product);
+  const page = Number(req.query.page) || 1;
+  const limit = 10;
+  const skip = (page - 1) * limit;
+
+  const search = req.query.search
+    ?{
+      $or: [
+        { name: { $regex: req.query.search, $options: "i" } },
+        { description: { $regex: req.query.search, $options: "i" } }
+      ]}
+    : {};
+  
+  const totalItems = await Product.countDocuments(search);
+  const product = await Product.find(search).skip(skip).limit(limit);
+  res.json({
+    page,
+    limit,
+    totalItems,
+    totalPages: Math.ceil(totalItems / limit),
+    product
+  });
 });
 
 // Get product by ID
 export const getProductId = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
+  const seller = await User.findById(product.sellerId).select('name');
   if (!product) {
     res.status(404);
     throw new Error('Producto no encontrado');
   }
-  res.json(product);
+  res.json({product, seller});
 });
 
 // Update product
@@ -62,7 +83,6 @@ export const deleteProduct = asyncHandler(async (req, res) => {
   await product.deleteOne();
   res.json({ message: 'Producto eliminado' });
 });
-// Get my products
 
 // Get my products
 export const myProducts = asyncHandler(async (req, res) => {
